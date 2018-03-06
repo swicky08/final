fbec4d33012e44410b010be2f8cd09c3a2f9a6b2
swicky08 <swicky08@gmail.com>
2018-03-06 15:55:00 -0600

    Completed

--- App.js
+++ App.js
@@ -1,23 +1,158 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { ListView, StyleSheet, View } from 'react-native';
+import { Body, Title, Right, Container, Header, Content, Button, Icon, List, ListItem, Text } from 'native-base';
 
 export default class App extends React.Component {
-  render() {
+  constructor() {
+    super();
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      pints: []
+    }
+  }
+
+  // Retrieve the list of flavors from Airtable
+  getFlavors() {
+    // Airtable API endpoint, replace with your own
+    let airtableUrl = "https://api.airtable.com/v0/appmePw6hSVObpF42/icecreamconsumption?&view=Grid%20view";
+
+    // Needed for Airtable authorization, replace with your own API key
+    let requestOptions = {
+      headers: new Headers({
+        'Authorization': 'Bearer key9VtNRJJD3fDGNd'
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.setState({
+        pints: json.records
+      });
+    });
+  }
+
+  // Runs when the application loads (i.e. the "App" component "mounts")
+  componentDidMount() {
+    this.getFlavors(); // refresh the list when we're done
+  }
+
+  // Upvote an idea
+  upvoteIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/appmePw6hSVObpF42/icecreamconsumption/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer key9VtNRJJD3fDGNd', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          pints: data.fields.pints + 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getFlavors(); // refresh the list when we're done
+    });
+  }
+
+  // Downvote an idea
+  downvoteIdea(data, secId, rowId, rowMap) {
+    // Slide the row back into place
+    rowMap[`${secId}${rowId}`].props.closeRow();
+
+    // Airtable API endpoint
+    let airtableUrl = "https://api.airtable.com/v0/appmePw6hSVObpF42/icecreamconsumption/" + data.id;
+
+    // Needed for Airtable authorization
+    let requestOptions = {
+      method: 'PATCH',
+      headers: new Headers({
+        'Authorization': 'Bearer key9VtNRJJD3fDGNd', // replace with your own API key
+        'Content-type': 'application/json'
+      }),
+      body: JSON.stringify({
+        fields: {
+          pints: data.fields.pints - 1
+        }
+      })
+    };
+
+    // Form the request
+    let request = new Request(airtableUrl, requestOptions);
+
+    // Make the request
+    fetch(request).then(response => response.json()).then(json => {
+      this.getFlavors(); // refresh the list when we're done
+    });
+  }
+
+  // The UI for each row of data
+  renderRow(data) {
     return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+      <ListItem style={{ paddingLeft: 20, paddingRight: 20 }}>
+        <Body>
+          <Text>{data.fields.flavor}</Text>
+        </Body>
+        <Right>
+          <Text note>{data.fields.pints} pints</Text>
+        </Right>
+      </ListItem>
+    )
+  }
+
+  // The UI for what appears when you swipe right
+  renderSwipeRight(data, secId, rowId, rowMap) {
+    return (
+      <Button full success onPress={() => this.upvoteIdea(data, secId, rowId, rowMap)}>
+        <Icon active name="arrow-up" />
+      </Button>
+    )
+  }
+
+  // The UI for what appears when you swipe left
+  renderSwipeLeft(data, secId, rowId, rowMap) {
+    return (
+      <Button full danger onPress={() => this.downvoteIdea(data, secId, rowId, rowMap)}>
+        <Icon active name="arrow-down" />
+      </Button>
+    )
+  }
+
+  render() {
+    let rows = this.ds.cloneWithRows(this.state.pints);
+    return (
+      <Container>
+        <Header>
+          <Body>
+            <Title>How much ice cream have I eaten</Title>
+          </Body>
+        </Header>
+        <Content>
+          <List
+            dataSource={rows}
+            renderRow={(data) => this.renderRow(data)}
+            renderLeftHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeRight(data, secId, rowId, rowMap)}
+            renderRightHiddenRow={(data, secId, rowId, rowMap) => this.renderSwipeLeft(data, secId, rowId, rowMap)}
+            leftOpenValue={75}
+            rightOpenValue={-75}
+          />
+        </Content>
+      </Container>
     );
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
